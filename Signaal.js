f585d3f5e1205b7f4a9b547861888a64c62d5ab6
samvanasselt <sam@samvanasselt.nl>
2016-06-01 22:00:57 +0200

    FlipFlop

--- Signaal.js
+++ Signaal.js
@@ -37,8 +37,9 @@ function cComponent(pKind, pDelay = 0, pPower = 0) {
     var PowerLastDisplay;
     var Display;
     var Power = [];
+    var SourceNames = [];
     var Sources = [];
-    
+
     this.Kind = pKind;
     this.Display = false;
     this.PowerLastDisplay = 0;
@@ -52,7 +53,15 @@ cComponent.prototype.Reset = function (pDelay = 0, pPower = 0) {
 cComponent.prototype.SetSources = function () {
     var args = Array.from(arguments);
     this.Sources = [];
-    for (var i = 0; i < args.length; i++) { this.Sources.push(gComponents[ComponentIndex(args[i])]); }
+    if (args.length > 0) {
+        this.SourceNames = [];
+        for (var i = 0; i < args.length; i++) { this.SourceNames.push(args[i]); }
+    }
+    for (var i = 0; i < this.SourceNames.length; i++) {
+        var lIndex = ComponentIndex(this.SourceNames[i]);
+        if (lIndex < 0) { ShowError('Geen component ' + this.SourceNames[i]); }
+        else            { this.Sources.push(gComponents[lIndex]); }
+    }
 }
 cComponent.prototype.Tick = function () {
     for (var i = 0; i < this.Delay; i++) {this.Power[i] = this.Power[i+1];}
@@ -90,8 +99,9 @@ cComponent.prototype.ErrorMessage = function (pError) {
      ShowError(lHTML);
 }
 // ========================================================================== //
-function cInverter(pName) {
+function cInverter(pName, pSourceNames) {
     this.Name = pName;
+    this.SourceNames = (pSourceNames == undefined) ? [] : pSourceNames;
     this.Reset(1, 15);
 }
 cInverter.prototype = new cComponent('inverter', 1, 15);
@@ -107,17 +117,26 @@ cInverter.prototype.Dump = function () {
      return lHTML;
 }
 // ========================================================================== //
-function cRepeater(pName, pDelay) {
+function cRepeater(pName, pSourceNames, pDelay) {
     this.Name = pName;
+    // this.Locked = false;
+    this.SourceNames = (pSourceNames == undefined) ? [] : pSourceNames;
     this.Reset(pDelay, 0);
 }
 cRepeater.prototype = new cComponent('repeater', 1, 0);
 cRepeater.prototype.SetInput = function () {
     try {
         var lLocked = false;
-        if (this.Sources.length > 2) { lLocked = (this.Sources[2].Power[0] > 0);}
-        if (this.Sources.length > 1) { lLocked = (this.Sources[1].Power[0] > 0);}
-        if (!lLocked) {this.Power[this.Delay] = (this.Sources[0].Power[0] == 0) ?  0 : 15;}
+        for (var lLock = 1; lLock <= this.Sources.length - 1; lLock++) {
+            for (var s = 0; s <= this.Sources[lLock].Delay; s++) {
+                lLocked = lLocked || (this.Sources[lLock].Power[s] > 0);
+            }
+        }
+        if (this.Sources.length > 2) { lLocked = lLocked || (this.Sources[2].Power[this.Sources[2].Delay] > 0);}
+        if (this.Sources.length > 1) { lLocked = lLocked || (this.Sources[1].Power[this.Sources[1].Delay] > 0);}
+        if (lLocked)     { this.Locked = lLocked; }
+        // if (this.Locked) { this.Locked = lLocked; }
+        else             { this.Power[this.Delay] = (this.Sources[0].Power[0] == 0) ?  0 : 15; }
     }
     catch (err) { this.ErrorMessage(err); }
 }
@@ -133,8 +152,9 @@ cWire.prototype.SetInput = function () {
     this.Power[this.Delay] = (this.Sources[0].Power[0] > this.Wirelength) ? this.Sources[0].Power[0] - this.Wirelength : 0;
 }
 // ========================================================================== //
-function cOr(pName) {
+function cOr(pName, pSourceNames) {
     this.Name = pName;
+    this.SourceNames = (pSourceNames == undefined) ? [] : pSourceNames;
     this.Reset(0, 0);
 }
 cOr.prototype = new cComponent('or', 0, 0);
@@ -148,6 +168,7 @@ cOr.prototype.SetInput = function () {
 // ========================================================================== //
 function cLever(pName) {
     this.Name = pName;
+    this.SourceNames = [];
     this.Reset(0, 0);
 }
 cLever.prototype = new cComponent('lever', 0, 0);
@@ -170,126 +191,124 @@ function PowerLine(pPower, pLength) {
     return lPower;
 }
 // ========================================================================== //
-function AddInstructionClockUnit(pUnitnr, pSource = '') {
-    var lPrefix =  'IC.' + pUnitnr + '.';
-    var nPrefix = '-IC.' + pUnitnr + '.';
-    gComponents.push(new cRepeater( lPrefix + 's.Lock', 1));
-    gComponents.push(new cRepeater( lPrefix + 't.Lock', 1));
-    gComponents.push(new cRepeater( lPrefix + 's'     , 1));
-    gComponents.push(new cInverter( nPrefix + 's'        ));
-    gComponents.push(new cOr      ( nPrefix + 't.OR'     ));
-    gComponents.push(new cInverter( lPrefix + 't.AND'    ));
-    gComponents.push(new cRepeater( lPrefix + 't'     , 1));
-    gComponents.push(new cInverter( nPrefix + 't'        ));
-    gComponents.push(new cOr      ( nPrefix + 'T'        ));
-    gComponents.push(new cInverter( lPrefix + 'T'        ));
-    var lSource = pSource;
-    if (lSource == '') { lSource = 'IC.' + (pUnitnr - 1) + '.t'; }
-    gComponents[ComponentIndex( lPrefix + 's.Lock')].SetSources('-Phi-0');
-    gComponents[ComponentIndex( lPrefix + 't.Lock')].SetSources('-Phi-1');
-    gComponents[ComponentIndex( lPrefix + 's'     )].SetSources(lSource, lPrefix + 's.Lock');
-    gComponents[ComponentIndex( nPrefix + 's'     )].SetSources(lPrefix + 's');
-    gComponents[ComponentIndex( nPrefix + 't.OR'  )].SetSources(nPrefix + 's', 'RST');
-    gComponents[ComponentIndex( lPrefix + 't.AND' )].SetSources(nPrefix + 't.OR');
-    gComponents[ComponentIndex( lPrefix + 't'     )].SetSources(lPrefix + 't.AND', lPrefix + 't.Lock');
-    gComponents[ComponentIndex( nPrefix + 't'     )].SetSources(lPrefix + 't');
-    gComponents[ComponentIndex( nPrefix + 'T'     )].SetSources(nPrefix + 't','CK.4.r');
-    gComponents[ComponentIndex( lPrefix + 'T'     )].SetSources(nPrefix + 'T');
-
-    gComponents[ComponentIndex(lPrefix + 'T')].Display = true;
-}
-// ========================================================================== //
 function ClockInit() {
     var lRepname = '';
     var lSource = '';
-    gComponents.push(new cInverter('CK'));
+    gComponents.push(new cInverter('CK',['CK.4.r']));
     for (var r = 1; r <= 4; r++) {
         lRepname = 'CK.' + r + '.r';
         lSource = (r == 1) ? 'CK' : 'CK.' + (r-1) + '.r';
-        gComponents.push(new cRepeater(      lRepname, 1)); gComponents[ComponentIndex(      lRepname)].SetSources(lSource);
-        gComponents.push(new cInverter('-' + lRepname   )); gComponents[ComponentIndex('-' + lRepname)].SetSources(lRepname);
+        gComponents.push(new cRepeater(      lRepname, [lSource ], 1 ));
+        gComponents.push(new cInverter('-' + lRepname, [lRepname]    ));
     }
-    gComponents[ComponentIndex('CK')].SetSources('CK.4.r');
     //  Afgeleide clocksignalen
-    gComponents.push(new cOr(      'CK.p0'     )); gComponents[ComponentIndex( 'CK.p0'  ) ].SetSources( 'CK.1.r', 'CK.2.r', 'CK.3.r');
-    gComponents.push(new cOr(      'CK.p1'     )); gComponents[ComponentIndex( 'CK.p1'  ) ].SetSources('-CK.1.r','-CK.2.r','-CK.3.r');
-    gComponents.push(new cRepeater('CK.p0+1', 1)); gComponents[ComponentIndex( 'CK.p0+1') ].SetSources( 'CK.p0'  );
-    gComponents.push(new cInverter('Phi-0'     )); gComponents[ComponentIndex( 'Phi-0'  ) ].SetSources( 'CK.p0+1');
-    gComponents.push(new cInverter('Phi-1'     )); gComponents[ComponentIndex( 'Phi-1'  ) ].SetSources( 'CK.p1'  );
-    gComponents.push(new cInverter('-Phi-0'    )); gComponents[ComponentIndex('-Phi-0'  ) ].SetSources( 'Phi-0'  );
-    gComponents.push(new cInverter('-Phi-1'    )); gComponents[ComponentIndex('-Phi-1'  ) ].SetSources( 'Phi-1'  );
-    gDisplay = ['CK','Phi-0','Phi-1'];
+    gComponents.push(new cOr(      'CK.p0'  , [ 'CK.1.r', 'CK.2.r', 'CK.3.r']));
+    gComponents.push(new cOr(      'CK.p1'  , ['-CK.1.r','-CK.2.r','-CK.3.r']));
+    gComponents.push(new cRepeater('CK.p0+1', ['CK.p0'  ], 1));
+    gComponents.push(new cInverter('Phi-0'  , ['CK.p0+1']   ));
+    gComponents.push(new cInverter('Phi-1'  , ['CK.p1'  ]   ));
+    gComponents.push(new cInverter('-Phi-0' , ['Phi-0'  ]   ));
+    gComponents.push(new cInverter('-Phi-1' , ['Phi-1'  ]   ));
 }
 function ClockSignals() {
     gComponents = [];
     ClockInit();
-    Signaal();    
+    gDisplay = ['CK','Phi-0','Phi-1'];
+    Signaal();
 }
 // ========================================================================== //
+function AddInstructionClockUnit(pUnitnr, pSource = '') {
+    var lPrefix =  'IC.' + pUnitnr + '.';
+    var nPrefix = '-IC.' + pUnitnr + '.';
+    var lSource = pSource;
+    if (lSource == '') { lSource = 'IC.' + (pUnitnr - 1) + '.t'; }
+    gComponents.push(new cRepeater( lPrefix + 's.Lock', ['-Phi-0'                             ], 1));
+    gComponents.push(new cRepeater( lPrefix + 't.Lock', ['-Phi-1'                             ], 1));
+    gComponents.push(new cRepeater( lPrefix + 's'     , [lSource          , lPrefix + 's.Lock'], 1));
+    gComponents.push(new cInverter( nPrefix + 's'     , [lPrefix + 's'                        ]   ));
+    gComponents.push(new cOr      ( nPrefix + 't.OR'  , [nPrefix + 's'    , 'RST'             ]   ));
+    gComponents.push(new cInverter( lPrefix + 't.AND' , [nPrefix + 't.OR'                     ]   ));
+    gComponents.push(new cRepeater( lPrefix + 't'     , [lPrefix + 't.AND', lPrefix + 't.Lock'], 1));
+    gComponents.push(new cInverter( nPrefix + 't'     , [lPrefix + 't'                        ]   ));
+    gComponents.push(new cOr      ( nPrefix + 'T'     , [nPrefix + 't'    ,'CK.4.r'           ]   ));
+    gComponents.push(new cInverter( lPrefix + 'T'     , [nPrefix + 'T'                        ]   ));
+    gComponents[ComponentIndex(lPrefix + 'T')].Display = true;
+}
 function InstructionClockInit() {
     ClockInit();
-    gComponents.push(new cLever('RST'));
-    // gComponents.push(new cInverter('-RST'));
-    gComponents.push(new cRepeater('IC.0.t.Lock', 1));
-    gComponents.push(new cRepeater('IC.0.t', 1));
-    
-    gComponents[ComponentIndex('RST') ].SetSources('RST');
-    // gComponents[ComponentIndex('-RST') ].SetSources('RST');
-    gComponents[ComponentIndex('IC.0.t.Lock') ].SetSources('-Phi-1');
-    gComponents[ComponentIndex('IC.0.t') ].SetSources('RST', 'IC.0.t.Lock');
-
-    gComponents.push(new cInverter('-IC.0.t')); gComponents[ComponentIndex('-IC.0.t')].SetSources( 'IC.0.t');
-    gComponents.push(new cOr      ('-IC.0.T')); gComponents[ComponentIndex('-IC.0.T')].SetSources('-IC.0.t','CK.4.r');
-    gComponents.push(new cInverter( 'IC.0.T')); gComponents[ComponentIndex( 'IC.0.T')].SetSources('-IC.0.T');
-
+    gComponents.push(new cLever   ( 'RST'));
+    gComponents.push(new cInverter('-RST', ['RST']));
+    gComponents.push(new cRepeater( 'IC.0.t.Lock', ['-Phi-1'               ], 1));
+    gComponents.push(new cRepeater( 'IC.0.t'     , [ 'RST'  , 'IC.0.t.Lock'], 1));
+    gComponents.push(new cInverter('-IC.0.t', [ 'IC.0.t'         ]));
+    gComponents.push(new cOr      ('-IC.0.T', ['-IC.0.t','CK.4.r']));
+    gComponents.push(new cInverter( 'IC.0.T', ['-IC.0.T'         ]));
     AddInstructionClockUnit(1);
     AddInstructionClockUnit(2);
     AddInstructionClockUnit(3);
     AddInstructionClockUnit(4);
     AddInstructionClockUnit(5);
+    gComponents.push(new cRepeater('IC.PreRST.Lock', ['-Phi-0'] ));
+    gComponents.push(new cRepeater('IC.RST.Lock'   , ['-Phi-1'] ));
+    gComponents.push(new cOr      ('IC.5.t or RST' , ['IC.5.t'       , 'RST'] ));
+    gComponents.push(new cRepeater('IC.PreRST'     , ['IC.5.t or RST', 'IC.PreRST.Lock']));
+    gComponents.push(new cRepeater('IC.RST'        , ['IC.PreRST'    , 'IC.RST.Lock'   ]));
 
-    gComponents.push(new cRepeater('IC.PreRST.Lock' ));  gComponents[ComponentIndex('IC.PreRST.Lock' )].SetSources('-Phi-0');
-    gComponents.push(new cRepeater('IC.RST.Lock'    ));  gComponents[ComponentIndex('IC.RST.Lock'    )].SetSources('-Phi-1');
-    gComponents.push(new cOr      ('IC.5.t or RST'  ));  gComponents[ComponentIndex('IC.5.t or RST'  )].SetSources('IC.5.t'       , 'RST'           );
-    gComponents.push(new cRepeater('IC.PreRST'      ));  gComponents[ComponentIndex('IC.PreRST'      )].SetSources('IC.5.t or RST', 'IC.PreRST.Lock');
-    gComponents.push(new cRepeater('IC.RST'         ));  gComponents[ComponentIndex('IC.RST'         )].SetSources('IC.PreRST'    , 'IC.RST.Lock'   );
     gComponents[ComponentIndex('IC.0.t') ].SetSources('IC.PreRST', 'IC.0.t.Lock');
     gComponents[ComponentIndex('RST')].On();
-
-    gDisplay = ['CK','Phi-0','Phi-1','RST','IC.RST','IC.0.T','IC.1.T','IC.2.T','IC.3.T','IC.4.T','IC.5.T'];
-    // gDisplay = ['CK','Phi-0','Phi-1','RST','IC.0.T','IC.1.T'];
 }
 function InstructionClockSignals() {
     gComponents = [];
     InstructionClockInit();
-    Signaal();    
+    gDisplay = ['CK','Phi-0','Phi-1','RST','IC.RST','IC.0.T','IC.1.T','IC.2.T','IC.3.T','IC.4.T','IC.5.T'];
+    Signaal();
 }
 // ========================================================================== //
 function LockedRepeaterInit() {
-    gComponents.push(new cRepeater('LR.Lock', 1)); gComponents[ComponentIndex('LR.Lock')].SetSources('CK.1.r');
-    gComponents.push(new cRepeater('LR', 1));      gComponents[ComponentIndex('LR'     )].SetSources('CK.1.r', 'LR.Lock');
-    gComponents.push(new cRepeater('NR', 1));      gComponents[ComponentIndex('NR'     )].SetSources('CK.2.r');
-    gDisplay = ['CK','CK.1.r','LR.Lock','LR','NR','CK.2.r'];
+    gComponents.push(new cRepeater('LR.Lock', ['CK.1.r'           ], 1));
+    gComponents.push(new cRepeater('LR'     , ['CK.1.r', 'LR.Lock'], 1));
+    gComponents.push(new cRepeater('NR'     , ['CK.2.r'           ], 1));
 }
 function LockedRepeaterSignals() {
     gComponents = [];
     ClockInit();
     LockedRepeaterInit();
-    Signaal();    
+    gDisplay = ['CK','CK.1.r','LR.Lock','LR','NR','CK.2.r'];
+    Signaal();
 }
+// ========================================================================== //
+function FlipFlopInit() {
+    gComponents.push(new cInverter( 'FF.In'     , [ 'CK'   ]   ));
+    gComponents.push(new cRepeater( 'FF.R1.Lock', [ 'CK'   ], 1));
+    gComponents.push(new cRepeater( 'FF.R2.Lock', [ 'FF.In'], 1));
+    gComponents.push(new cRepeater( 'FF.R1'     , ['-FF.R2', 'FF.R1.Lock'], 1));
+    gComponents.push(new cRepeater( 'FF.R2'     , [ 'FF.R1', 'FF.R2.Lock'], 1));
+    gComponents.push(new cInverter('-FF.R2'     , [ 'FF.R2']   ));
+}
+function FlipFlopSignals() {
+    gComponents = [];
+    ClockInit();
+    FlipFlopInit();
+    gDisplay = ['CK', 'FF.In', 'FF.R1.Lock', 'FF.R2.Lock', 'FF.R1', 'FF.R2', '-FF.R2'];
+    Signaal();
+}
+// ========================================================================== //
 function CheckComponentsAndDisplay() {
-    for (var i = 0; i < gComponents.length; i++) { 
-        if (gComponents[i].Power   == undefined) { gComponents[i].ErrorMessage('Geen stroom');}
-        if (gComponents[i].Sources == undefined) { gComponents[i].ErrorMessage('Geen bron');}
+    for (var i = 0; i < gComponents.length; i++) {
+        if (gComponents[i].Power       == undefined) { gComponents[i].ErrorMessage('Geen stroom');}
+        if (gComponents[i].SourceNames == undefined) { gComponents[i].ErrorMessage('Geen bron');}
+        gComponents[i].SetSources();
+        if (gComponents[i].Sources     == undefined) { gComponents[i].ErrorMessage('Geen bron');}
     }
-    for (var i = 0; i < gDisplay.length; i++) { 
+    for (var i = 0; i < gDisplay.length; i++) {
         if (ComponentIndex(gDisplay[i]) < 0) { ShowError('Geen component ' + gDisplay[i]); }
     }
 }
+// ========================================================================== //
 function Signaal() {
     initCanvas();
     CheckComponentsAndDisplay();
-    for (var i = 0; i < gDisplay.length; i++) { 
+    for (var i = 0; i < gDisplay.length; i++) {
         try { gComponents[ComponentIndex(gDisplay[i])].Display = true; }
         catch (err) { console.log(gDisplay[i] + ' ' + err); }
     }
@@ -302,6 +321,7 @@ function Signaal() {
     for (t = 0; t < 140; t++) {
         var lRow = 1;
         if (t == 10 && ComponentIndex('RST') > 0) { gComponents[ComponentIndex('RST')].Off(); }
+        if (t == 14 && ComponentIndex('LR' ) > 0) { gComponents[ComponentIndex('LR')].Reset(1,15); }
         for (var i = 0; i < gComponents.length; i++) { gComponents[i].SetInput();  }
         for (var i = 0; i < gDisplay.length; i++) { lRow = gComponents[ComponentIndex(gDisplay[i])].Draw(t,lRow); }
         for (var i = 0; i < gComponents.length; i++) { gComponents[i].Tick();      }
